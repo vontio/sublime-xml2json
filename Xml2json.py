9827be6667494be697e5065a87fd441a5e8b04fc
vontio <vontio@vontios-iMac.local>
2014-08-27 18:46:49 +0800

    fix disorder in sublime 2

--- Xml2json.py
+++ Xml2json.py
@@ -1,9 +1,14 @@
 import sublime, sublime_plugin
-import json
+
 if sublime.version() >= '3000':
+	from collections import OrderedDict
+	import json
 	from . import xmltodict
 else:
 	import xmltodict
+	import simplejson as json
+	from ordereddict import OrderedDict
+
 def newViewWithText(text):
 	newView = sublime.active_window().new_file()
 	if sublime.version() >= '3000':
@@ -27,12 +32,12 @@ class Json2xmlCommand(sublime_plugin.TextCommand):
 	def run(self,edit):
 		fulltext = self.view.substr(sublime.Region(0, self.view.size()))
 		try:
-			jsonObj = json.loads(fulltext)
+			jsonObj = json.JSONDecoder(object_pairs_hook=OrderedDict).decode(fulltext)
 			xmlStr = xmltodict.unparse(jsonObj)
 		except ValueError:
 			try:
-				newText = '{"root":' + fulltext + '}'
-				jsonObj = json.loads(newText) #try to add a wrapper
+				newText = '{"root":' + fulltext + '}' #try to add a wrapper
+				jsonObj = json.JSONDecoder(object_pairs_hook=OrderedDict).decode(fulltext)
 				xmlStr = xmltodict.unparse(jsonObj)
 			except Exception as e:
 				newViewWithText(newText)
--- xmltodict.py
+++ xmltodict.py
@@ -4,35 +4,38 @@
 from xml.parsers import expat
 from xml.sax.saxutils import XMLGenerator
 from xml.sax.xmlreader import AttributesImpl
-try: # pragma no cover
+try:  # pragma no cover
     from cStringIO import StringIO
-except ImportError: # pragma no cover
+except ImportError:  # pragma no cover
     try:
         from StringIO import StringIO
     except ImportError:
         from io import StringIO
-try: # pragma no cover
+try:  # pragma no cover
     from collections import OrderedDict
-except ImportError: # pragma no cover
-    try: 
+except ImportError:  # pragma no cover
+    try:
         from ordereddict import OrderedDict
     except ImportError:
         OrderedDict = dict
 
-try: # pragma no cover
+try:  # pragma no cover
     _basestring = basestring
-except NameError: # pragma no cover
+except NameError:  # pragma no cover
     _basestring = str
-try: # pragma no cover
+try:  # pragma no cover
     _unicode = unicode
-except NameError: # pragma no cover
+except NameError:  # pragma no cover
     _unicode = str
 
 __author__ = 'Martin Blech'
-__version__ = '0.6.0'
+__version__ = '0.9.0'
 __license__ = 'MIT'
 
-class ParsingInterrupted(Exception): pass
+
+class ParsingInterrupted(Exception):
+    pass
+
 
 class _DictSAXHandler(object):
     def __init__(self,
@@ -45,7 +48,9 @@ class _DictSAXHandler(object):
                  cdata_separator='',
                  postprocessor=None,
                  dict_constructor=OrderedDict,
-                 strip_whitespace=True):
+                 strip_whitespace=True,
+                 namespace_separator=':',
+                 namespaces=None):
         self.path = []
         self.stack = []
         self.data = None
@@ -60,9 +65,30 @@ class _DictSAXHandler(object):
         self.postprocessor = postprocessor
         self.dict_constructor = dict_constructor
         self.strip_whitespace = strip_whitespace
+        self.namespace_separator = namespace_separator
+        self.namespaces = namespaces
 
-    def startElement(self, name, attrs):
-        attrs = self.dict_constructor(zip(attrs[0::2], attrs[1::2]))
+    def _build_name(self, full_name):
+        if not self.namespaces:
+            return full_name
+        i = full_name.rfind(self.namespace_separator)
+        if i == -1:
+            return full_name
+        namespace, name = full_name[:i], full_name[i+1:]
+        short_namespace = self.namespaces.get(namespace, namespace)
+        if not short_namespace:
+            return name
+        else:
+            return self.namespace_separator.join((short_namespace, name))
+
+    def _attrs_to_dict(self, attrs):
+        if isinstance(attrs, dict):
+            return attrs
+        return self.dict_constructor(zip(attrs[0::2], attrs[1::2]))
+
+    def startElement(self, full_name, attrs):
+        name = self._build_name(full_name)
+        attrs = self._attrs_to_dict(attrs)
         self.path.append((name, attrs or None))
         if len(self.path) > self.item_depth:
             self.stack.append((self.item, self.data))
@@ -75,7 +101,8 @@ class _DictSAXHandler(object):
             self.item = attrs or None
             self.data = None
 
-    def endElement(self, name):
+    def endElement(self, full_name):
+        name = self._build_name(full_name)
         if len(self.path) == self.item_depth:
             item = self.item
             if item is None:
@@ -124,7 +151,9 @@ class _DictSAXHandler(object):
             item[key] = data
         return item
 
-def parse(xml_input, encoding='utf-8', expat=expat, *args, **kwargs):
+
+def parse(xml_input, encoding=None, expat=expat, process_namespaces=False,
+          namespace_separator=':', **kwargs):
     """Parse the given XML input and convert it into a dictionary.
 
     `xml_input` can either be a `string` or a file-like object.
@@ -135,6 +164,7 @@ def parse(xml_input, encoding='utf-8', expat=expat, *args, **kwargs):
 
     Simple example::
 
+        >>> import xmltodict
         >>> doc = xmltodict.parse(\"\"\"
         ... <a prop="x">
         ...   <b>1</b>
@@ -170,9 +200,9 @@ def parse(xml_input, encoding='utf-8', expat=expat, *args, **kwargs):
         path:[(u'a', {u'prop': u'x'}), (u'b', None)] item:1
         path:[(u'a', {u'prop': u'x'}), (u'b', None)] item:2
 
-    The optional argument `postprocessor` is a function that takes `path`, `key`
-    and `value` as positional arguments and returns a new `(key, value)` pair
-    where both `key` and `value` may have changed. Usage example::
+    The optional argument `postprocessor` is a function that takes `path`,
+    `key` and `value` as positional arguments and returns a new `(key, value)`
+    pair where both `key` and `value` may have changed. Usage example::
 
         >>> def postprocessor(path, key, value):
         ...     try:
@@ -191,19 +221,33 @@ def parse(xml_input, encoding='utf-8', expat=expat, *args, **kwargs):
         OrderedDict([(u'a', u'hello')])
 
     """
-    handler = _DictSAXHandler(*args, **kwargs)
-    parser = expat.ParserCreate()
-    parser.ordered_attributes = True
+    handler = _DictSAXHandler(namespace_separator=namespace_separator,
+                              **kwargs)
+    if isinstance(xml_input, _unicode):
+        if not encoding:
+            encoding = 'utf-8'
+        xml_input = xml_input.encode(encoding)
+    if not process_namespaces:
+        namespace_separator = None
+    parser = expat.ParserCreate(
+        encoding,
+        namespace_separator
+    )
+    try:
+        parser.ordered_attributes = True
+    except AttributeError:
+        # Jython's expat does not support ordered_attributes
+        pass
     parser.StartElementHandler = handler.startElement
     parser.EndElementHandler = handler.endElement
     parser.CharacterDataHandler = handler.characters
+    parser.buffer_text = True
     try:
         parser.ParseFile(xml_input)
     except (TypeError, AttributeError):
-        if isinstance(xml_input, _unicode):
-            xml_input = xml_input.encode(encoding)
         parser.Parse(xml_input, True)
     return handler.item
+
 
 def _emit(key, value, content_handler,
           attr_prefix='@',
@@ -240,20 +284,26 @@ def _emit(key, value, content_handler,
                 attrs[ik[len(attr_prefix):]] = iv
                 continue
             children.append((ik, iv))
-        if pretty and depth:
-            content_handler.ignorableWhitespace(newl + indent * depth)
+        if pretty:
+            content_handler.ignorableWhitespace(depth * indent)
         content_handler.startElement(key, AttributesImpl(attrs))
+        if pretty and children:
+            content_handler.ignorableWhitespace(newl)
         for child_key, child_value in children:
             _emit(child_key, child_value, content_handler,
                   attr_prefix, cdata_key, depth+1, preprocessor,
                   pretty, newl, indent)
         if cdata is not None:
             content_handler.characters(cdata)
+        if pretty and children:
+            content_handler.ignorableWhitespace(depth * indent)
         content_handler.endElement(key)
         if pretty and depth:
-            content_handler.ignorableWhitespace(newl + indent * (depth - 1))
+            content_handler.ignorableWhitespace(newl)
 
-def unparse(input_dict, output=None, encoding='utf-8', **kwargs):
+
+def unparse(input_dict, output=None, encoding='utf-8', full_document=True,
+            **kwargs):
     """Emit an XML document for the given `input_dict` (reverse of `parse`).
 
     The resulting XML document is returned as a string, but if `output` (a
@@ -270,22 +320,24 @@ def unparse(input_dict, output=None, encoding='utf-8', **kwargs):
     """
     ((key, value),) = input_dict.items()
     must_return = False
-    if output == None:
+    if output is None:
         output = StringIO()
         must_return = True
     content_handler = XMLGenerator(output, encoding)
-    content_handler.startDocument()
+    if full_document:
+        content_handler.startDocument()
     _emit(key, value, content_handler, **kwargs)
-    content_handler.endDocument()
+    if full_document:
+        content_handler.endDocument()
     if must_return:
         value = output.getvalue()
-        try: # pragma no cover
+        try:  # pragma no cover
             value = value.decode(encoding)
-        except AttributeError: # pragma no cover
+        except AttributeError:  # pragma no cover
             pass
         return value
 
-if __name__ == '__main__': # pragma: no cover
+if __name__ == '__main__':  # pragma: no cover
     import sys
     import marshal
 
